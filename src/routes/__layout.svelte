53f16daeffa8abfa0f44414f9e7c8bdf50b37524
Fernando de la Rosa <fernando.delarosa@alegra.com>
2021-10-02 20:45:39 -0500

    Metatags y favicons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,12 @@
 <script>
-	import { t, addMessages, init, getLocaleFromNavigator } from 'precompile-intl-runtime';
+	import { addMessages, init, getLocaleFromNavigator } from 'precompile-intl-runtime';
 	import { onMount } from 'svelte';
 	import { session } from '$app/stores';
 	import { dev } from '$app/env';
 
 	import { mixpanel } from '$lib/utils/mixpanel';
 	import BreakpointHelper from '$lib/utils/BreakpointHelper.svelte';
+	import Metatags from '$lib/utils/Metatags.svelte';
 
 	import en from '../locales/en';
 	import es from '../locales/es';
@@ -30,9 +31,9 @@
 	<slot />
 </div>
 
+<Metatags />
+
 <svelte:head>
-	<title>{$t('meta.title')}</title>
-	<meta name="description" content={$t('meta.description')} />
 	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
