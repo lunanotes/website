59700250fdf22dcac39461e744135e07d062dbaa
Fernando de la Rosa <fernando.delarosa@alegra.com>
2021-10-31 12:44:18 -0600

    YouTube™ trademark

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,10 +1,10 @@
 export default {
-	'cta.description': 'LunaNotes is the easiest way to take notes while watching YouTube videos',
-	'cta.title': 'Stop wasting your time on YouTube!',
+	'cta.description': 'LunaNotes is the easiest way to take notes while watching YouTube™ videos',
+	'cta.title': 'Stop wasting your time on YouTube™!',
 	'feature.annotate.title': 'Draw',
 	'feature.annotate.description': 'Take screenshots of the video and draw on it',
 	'feature.create.title': 'Create',
-	'feature.create.description': 'Add annotations on any YouTube video',
+	'feature.create.description': 'Add annotations on any YouTube™ video',
 	'feature.customize.title': 'Customize',
 	'feature.customize.description': 'Add colors, lists, bold and other styles',
 	'feature.sync.title': 'Backup',
@@ -14,8 +14,8 @@ export default {
 	'hero.button.text': 'Add to Chrome',
 	'hero.description':
 		'LunaNotes lets you take notes while watching any video. Never miss a highlight again!',
-	'hero.title': 'The best note taking tool for YouTube.',
+	'hero.title': 'The best note taking tool for YouTube™.',
 	'meta.description':
-		'The best note taking tool for YouTube. LunaNotes is the easiest and powerful Chrome Extension for taking notes while watching any YouTube video.',
-	'meta.title': 'LunaNotes - YouTube notes'
+		'The best note taking tool for YouTube™. LunaNotes is the easiest and powerful Chrome Extension for taking notes while watching any YouTube™ video.',
+	'meta.title': 'LunaNotes - take notes on YouTube™'
 };
--- src/locales/es.js
+++ src/locales/es.js
@@ -1,11 +1,11 @@
 export default {
 	'cta.description':
-		'LunaNotes es la forma más sencilla de tomar notas mientras ves videos en YouTube',
-	'cta.title': '¡Deja de perder el tiempo en YouTube!',
+		'LunaNotes es la forma más sencilla de tomar notas mientras ves videos en YouTube™',
+	'cta.title': '¡Deja de perder el tiempo en YouTube™!',
 	'feature.annotate.title': 'Anota',
 	'feature.annotate.description': 'Haz capturas del video y añade anotaciones sobre ella',
 	'feature.create.title': 'Crea',
-	'feature.create.description': 'Añade notas en cualquier video de YouTube',
+	'feature.create.description': 'Añade notas en cualquier video de YouTube™',
 	'feature.customize.title': 'Personaliza',
 	'feature.customize.description': 'Agrega colores, listas, negritas y otros estilos',
 	'feature.sync.title': 'Respalda',
@@ -15,8 +15,8 @@ export default {
 	'hero.button.text': 'Añadir a Chrome',
 	'hero.description':
 		'LunaNotes te permite tomar notas mientras ves cualquier vídeo. ¡No vuelvas a perderte un momento destacado!',
-	'hero.title': 'LunaNotes es la mejor herramienta para tomar notas en YouTube.',
+	'hero.title': 'LunaNotes es la mejor herramienta para tomar notas en YouTube™.',
 	'meta.description':
-		'La mejor herramienta para tomar notas en YouTube. LunaNotes es la extensión de Chrome más sencilla y potente para tomar notas mientras ves cualquier video en YouTube.',
-	'meta.title': 'LunaNotes - Notas en YouTube'
+		'La mejor herramienta para tomar notas en YouTube™. LunaNotes es la extensión de Chrome más sencilla y potente para tomar notas mientras ves cualquier video en YouTube™.',
+	'meta.title': 'LunaNotes - Toma notas en YouTube™'
 };
