e2d96a5e0436665fbf9bca489bc9148759720679
Fernando de la Rosa <fernando.delarosa@alegra.com>
2022-01-02 14:18:44 -0600

    Fixed menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,14 +1,28 @@
 <script>
-	export let segment;
+	import { page } from '$app/stores';
 	export let stories = [];
+
+	function isActive(story) {
+		if (story.full_slug === 'index' && !$page?.params?.slug) {
+			return 'active';
+		}
+
+		if (story.full_slug === $page?.params?.slug) {
+			return 'active';
+		}
+
+		return '';
+	}
 </script>
 
 <nav>
-	{#each stories as story}
-		<a href={story.full_slug} class={story.full_slug === segment ? 'selected' : ''}>
-			{story.name}
-		</a>
-	{/each}
+	{#key $page?.params?.slug}
+		{#each stories as story}
+			<a href={story.full_slug === 'index' ? '/' : story.full_slug} class={isActive(story)}>
+				{story.name}
+			</a>
+		{/each}
+	{/key}
 </nav>
 
 <style lang="scss">
@@ -18,7 +32,26 @@
 		a {
 			@apply font-medium;
 			@apply text-lg;
-			@apply text-slate-700;
+			@apply text-slate-600;
+
+			@apply transition-all;
+			@apply duration-200;
+			@apply transform;
+
+			@apply underline;
+			@apply underline-offset-4;
+
+			@apply decoration-2;
+			@apply decoration-transparent;
+			&.active,
+			&:hover {
+				@apply decoration-2;
+				@apply decoration-amber-300;
+			}
+
+			&:hover {
+				@apply text-slate-800;
+			}
 		}
 	}
 </style>
